70cee82e62f72eb16186fbdf73a917b7b4a6345d
Jason Turner <jasontisawesome@gmail.com>
2023-11-17 20:48:31 -0800

    Initial Commit

---
+++ GuinLLVM9/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GuinLLVM9
+//
+//  Created by Jason Turner on 11/17/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
